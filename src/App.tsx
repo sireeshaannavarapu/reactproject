d6bead35d7be6155aa3e86a0ba27dcd4c90d543b
sireeshaannavarapu <sireeshaannavarapu8@gmail.com>
2023-08-20 16:07:45 +0530

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,77 @@
-
-
+import './App.css'
+import React from 'react'
 
 function App() {
-  
+ 
+   const students=[{
+     'firstname':'Bharathi',
+     "Lastname":'Reddy',
+     'gender':'Female'
+   },
+   {
+    'firstname':'Divya',
+    "Lastname":'Naidu',
+    'gender':'Female'
+  },
+  {
+    'firstname':'Ravi',
+    "Lastname":'Reddy',
+    'gender':'Male'
+  }
+  ]
+  const [count,setcount]=React.useState(1)
+  function abc(){
+    setcount(count+1)
+  }
+  function xyz(){
+    setcount(count-1)
+  }
 
+ 
   return (
     <>
-      <h1>welcome edupoly</h1>
+      <h1>Count</h1>
+      <table border='2'>
+   
+   <thead>
+     <th>firstname</th>
+     <th>Lastname</th>
+     <th>Gender</th>
+   </thead>
+   <tbody>
+    {
+       students.map((a)=>{
+           return  <tr style={(a.gender=='Female')?{backgroundColor:'pink'}:{backgroundColor:'skyblue'}}><td>{a.firstname}</td><td>{a.Lastname}</td><td>{a.gender}</td></tr>
+        
+     })
+    }
+   
+   </tbody>
+</table>
+
+
+<h1>{count}</h1>
+    <button onClick={abc}>increment</button>
+    <button onClick={xyz}>decrement</button>
+  
+
+
+
+
+
+
+
+
+
+
+
+
     </>
   )
 }
 
 export default App
+
+
+
+
